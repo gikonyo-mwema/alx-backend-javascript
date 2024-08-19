f3c7dffd47e249879f12681bfc6b44d66570ec0f
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-08-19 19:50:30 +0300

    welcome

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,18 @@
-Welcome to Holberton School, what is your name?
-Bob
-Your name is: Bob
+// 1-stdin.js
+
+// Display the welcome message
+console.log("Welcome to Holberton School, what is your name?");
+
+// Read user input from stdin
+process.stdin.on('data', (data) => {
+  // Display the user's name
+  console.log(`Your name is: ${data.toString().trim()}`);
+
+  // End the process
+  process.exit();
+});
+
+// Display the closing message when the process ends
+process.on('exit', () => {
+  console.log("This important software is now closing");
+});
