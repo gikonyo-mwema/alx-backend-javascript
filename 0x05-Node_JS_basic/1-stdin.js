4c446add8650c63683fa82e0fa573d56592d9ec6
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-08-20 04:55:43 +0300

    full task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,16 @@
-// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Display the welcome message
-console.log("Welcome to Holberton School, what is your name?");
-
-// Read user input from stdin
-process.stdin.on('data', (data) => {
-  // Display the user's name
-  console.log(`Your name is: ${data.toString().trim()}`);
-
-  // End the process
-  process.exit();
-});
-
-// Display the closing message when the process ends
-process.on('exit', () => {
-  console.log("This important software is now closing");
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
--- 0x05-Node_JS_basic/3-3-read_file_async.js
+++ 0x05-Node_JS_basic/3-3-read_file_async.js
@@ -1,20 +1,14 @@
-const express = require('express');
-
 const { readFile } = require('fs');
-
-const app = express();
-const port = 1245;
 
 function countStudents(fileName) {
   const students = {};
   const fields = {};
   let length = 0;
   return new Promise((resolve, reject) => {
-    readFile(fileName, (err, data) => {
-      if (err) {
-        reject(err);
+    readFile(fileName, (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
       } else {
-        let output = '';
         const lines = data.toString().split('\n');
         for (let i = 0; i < lines.length; i += 1) {
           if (lines[i]) {
@@ -33,31 +27,16 @@ function countStudents(fileName) {
           }
         }
         const l = length - 1;
-        output += `Number of students: ${l}\n`;
+        console.log(`Number of students: ${l}`);
         for (const [key, value] of Object.entries(fields)) {
           if (key !== 'field') {
-            output += `Number of students in ${key}: ${value}. `;
-            output += `List: ${students[key].join(', ')}\n`;
+            console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
           }
         }
-        resolve(output);
+        resolve(data);
       }
     });
   });
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-app.get('/students', (req, res) => {
-  countStudents(process.argv[2].toString()).then((output) => {
-    res.send(['This is the list of our students', output].join('\n'));
-  }).catch(() => {
-    res.send('This is the list of our students\nCannot load the database');
-  });
-});
-
-app.listen(port, () => {
-});
-
-module.exports = app;
+module.exports = countStudents;
