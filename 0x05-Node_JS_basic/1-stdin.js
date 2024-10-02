bbc1eeede6f31aec4d67a522dc26cca32713d3e9
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-02 04:39:42 +0300

    display message

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
+process.stdin.on('data', (data) => {    
+    process.stdout.write(`Your name is: ${data}`);
+    process.exit(); // Exit the process after receiving the input
 });
 
 process.on('exit', () => {
-    console.log('This important software is now closing');
+    process.stdout.write('This important software is now closing\n');
 });
