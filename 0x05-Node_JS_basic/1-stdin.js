b65a1df35e043c7e345b474b59e30fbb095984dd
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-02 04:42:16 +0300

    stdin event listener to use 'readable' event instead of 'data' event

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {    
-    process.stdout.write(`Your name is: ${data}`);
-    process.exit(); // Exit the process after receiving the input
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
