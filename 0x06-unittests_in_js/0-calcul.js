57c7f55373c8ca23efe5bfdec0394b175837cf38
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-03 21:51:29 +0300

    mocha first test

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,7 @@
+function calculateNumber(a, b) {
+    // Round both numbers and return their sum
+    return Math.round(a) + Math.round(b);
+}
+
+// Export the function for testing
+module.exports = calculateNumber;
