1e0a42a9ef6568f13c72f8fafa1da271403d8fa5
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-04 04:26:22 +0300

    test script update

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,5 @@
 function calculateNumber(a, b) {
-    // Round both numbers and return their sum
     return Math.round(a) + Math.round(b);
 }
 
-// Export the function for testing
 module.exports = calculateNumber;
