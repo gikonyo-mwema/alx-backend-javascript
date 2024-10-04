9eea6edaaea41a6e7f5e16199af8fa4debe8c5b9
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-04 05:13:43 +0300

    revert

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -2,17 +2,18 @@ const calculateNumber = (type, a, b) => {
   const aRound = Math.round(a);
   const bRound = Math.round(b);
 
-  if (type === 'SUM') {
-    return aRound + bRound;
-  } else if (type === 'SUBTRACT') {
+  if (type === 'SUBTRACT') {
     return aRound - bRound;
-  } else if (type === 'DIVIDE') {
+  }
+
+  if (type === 'DIVIDE') {
     if (bRound === 0) {
       return 'Error';
     }
     return aRound / bRound;
   }
+
+  return aRound + bRound;
 };
 
-export default calculateNumber;
-
+module.exports = calculateNumber;
